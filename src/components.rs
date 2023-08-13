2dd0b9eb789c11af1835d3d5eca42ee548b4ab42
Kai Li <kylinju@gmail.com>
2023-08-13 07:55:19 -0400

    turn based move

--- src/components.rs
+++ src/components.rs
@@ -15,3 +15,8 @@ pub struct Enemy;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct MovingRandomly;
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantsToMove {
+    pub entity: Entity,
+    pub destination: Point,
+}
